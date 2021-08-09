369f38c77cbd80bd505fe371ad5eec014f0779dc
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-08-09 18:42:26 -0400

    add up and down margins for camera follower

--- Assets/Scripts/Camera/FollowEntity.cs
+++ Assets/Scripts/Camera/FollowEntity.cs
@@ -13,6 +13,10 @@ namespace CameraNS {
         public float LeftPercentage;
         [Range(0, 1)]
         public float RightPercentage;
+        [Range(0, 1)]
+        public float UpPercentage;
+        [Range(0, 1)]
+        public float DownPercentage;
 
         private Camera _camera;
         private Vector3 _target;
@@ -25,16 +29,27 @@ namespace CameraNS {
 
         private void Update() {
             Vector3 point = _camera.WorldToScreenPoint(EntityToFollow.transform.position);
+            if (float.IsInfinity(point.z)) {
+                return;
+            }
+
             float leftPc = point.x / Screen.width;
             float rightPc = 1 - leftPc;
+            float downPc = point.y / Screen.height;
+            float upPc = 1 - downPc;
 
+            _target = transform.position;
             if (leftPc < LeftPercentage) {
-                _target = transform.position;
                 _target.z -= (LeftPercentage - leftPc) * Screen.width / point.z;
             } else
             if (rightPc < RightPercentage) {
-                _target = transform.position;
                 _target.z += (RightPercentage - rightPc) * Screen.width / point.z;
+            } else
+            if (upPc < UpPercentage) {
+                _target.x -= (UpPercentage - upPc) * Screen.height / point.z;
+            } else
+            if (downPc < DownPercentage) {
+                _target.x += (DownPercentage - downPc) * Screen.height / point.z;
             }
 
             transform.position = Vector3.Lerp(transform.position, _target, Time.deltaTime * CameraSpeed);
