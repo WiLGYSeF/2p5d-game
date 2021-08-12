9ebc2c005813175d3ece67d2baaeaebe685ed311
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-08-12 15:49:35 -0400

    change to private set

--- Assets/Scripts/Entity/EntityDamageEvent.cs
+++ Assets/Scripts/Entity/EntityDamageEvent.cs
@@ -4,8 +4,8 @@ namespace EventSystemNS {
     public class EntityDamageEvent : System.EventArgs {
         public const string NAME = "EntityDamageEvent";
 
-        public Entity Entity {get; set;}
-        public float Damage {get; set;}
+        public Entity Entity {get; private set;}
+        public float Damage {get; private set;}
 
         public EntityDamageEvent(Entity entity, float damage) {
             Entity = entity;
